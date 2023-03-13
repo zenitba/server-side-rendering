9b17392170808b7df3bf5a46a08f21471a39a43d
zenitba <112856019+zenitba@users.noreply.github.com>
2023-03-13 12:34:39 +0100

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 import express from 'express'
 
+// basis url voor de api
 const url = 'https://api.vinimini.fdnd.nl/api/v1'
 
 // Maak een nieuwe express app
@@ -12,14 +13,33 @@ app.set('views', './views')
 app.use(express.static('public'))
 
 // Maak een route voor de index
+// dit plak je aan de basis url van de api, /categories
+// { categories: data.categories } toen het niet werkte, werkt nu wel met enkel data
 app.get('/', (request, response) => {
-    let categoriesUrl = url + '/categories'
-  
-    fetchJson(categoriesUrl).then((data) => {
-      response.render('index', data)
-    })
+  let categoriesUrl = url + '/categories'
 
+  fetchJson(categoriesUrl).then((data) => {
+    response.render('index', data)
   })
+})
+
+// dit plak je aan de basis url van de api, /producten
+app.get('/Ei', async (request, response) => {
+  let productenUrl = url + '/producten'
+
+  await fetchJson(productenUrl).then((data) => {
+    response.render('Ei', data )
+  })
+})
+
+// route naar pinda.ejs
+app.get('/Pinda', async (request, response) => {
+  let productenUrl = url + '/producten'
+
+  await fetchJson(productenUrl).then((data) => {
+    response.render('Pinda', data )
+  })
+})
 // Stel het poortnummer in en start express
 app.set('port', process.env.PORT || 8000)
 app.listen(app.get('port'), function () {
